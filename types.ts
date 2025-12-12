d49c68259f68d1ca377c4fac1e67548ef176588a
Josh Platt <jluxury929@gmail.com>
2025-12-12 15:33:35 -0500

    Update types.ts

--- types.ts
+++ types.ts
@@ -2,7 +2,6 @@
 
 import { BigNumber } from 'ethers';
 
-// FIX TS2305: Ensure Strategy is exported
 export interface Strategy {
     name: string;
     isActive: boolean;
@@ -16,7 +15,6 @@ export interface ChainConfig {
     flashbotsUrl: string;
 }
 
-// FIX TS2339 (ExecutionWorker.ts) - Added missing fields as they are used in the worker logic
 export interface WorkerTaskData {
     txHash: string;
     pendingTx: any;
@@ -24,7 +22,6 @@ export interface WorkerTaskData {
     mevHelperContractAddress: string; 
     txs: string[];
     blockNumber: number;
-    // ExecutionWorker.ts needs these to be numbers/BigNumbers for calculations
     maxPriorityFeePerGas: BigNumber; 
     maxFeePerGas: BigNumber; 
 }
@@ -37,15 +34,13 @@ export interface WorkerResult {
 
 export type TaskResolver = (result: WorkerResult) => void;
 
-// FIX TS2353/TS2339 (WorkerPool.ts) - Added 'task' property
 export interface WorkerTaskWrapper {
     id: number;
     data: WorkerTaskData;
-    resolver: TaskResolver; // FIX TS2551: Property is named 'resolver'
+    resolver: TaskResolver;
     task: string;
 }
 
-// FIX TS2353 (WorkerPool.ts) - Added 'totalWorkers' property
 export interface WorkerStats {
     workerId: number;
     tasksProcessed: number;
